68cdff8b674916b2f2d272354ce611a0da5613fc
AndreaOlivieri <oli.andrea17@gmail.com>
2014-05-17 01:14:18 +0200

    asd

--- 2014-05-16/python/exercise1.py
+++ 2014-05-16/python/exercise1.py
@@ -29,7 +29,7 @@ def REMOVE_CELL((V,CV),toRemove):
 	return V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
 
 def VMR_CELL(master, diagrams, toMerge, toRemove=[]):
-	#VIEW_CELL(master)
+	VIEW_CELL(master)
 	assert len(diagrams)==len(toMerge)
 
 	if(len(toMerge)>0 and len(toRemove)>0):
@@ -49,57 +49,60 @@ def VMR_CELL(master, diagrams, toMerge, toRemove=[]):
 #                                 MASTER
 ###############################################################################################
 
-shape = [5,3,2]
-sizePatterns = [ [__depthWall__,2.84,__depthWall__,6.24+__depthWall__,__depthWall__],
-                 [__depthWall__,12.23+2*__depthWall__,__depthWall__],
+shape = [2,1,2]
+sizePatterns = [ [__depthWall__+2.84, __depthWall__+6.24+__depthWall__+__depthWall__],
+                 [12.23+4*__depthWall__],
                  [__depthWall__,__heightFloor__]
                ]
 
-
 master = assemblyDiagramInit(shape)(sizePatterns)
+
+###############################################################################################
+#                                 LEFT_SIDE
+###############################################################################################
+
+shape = [2,3,1]
+sizePatterns = [ [__depthWall__,2.84],
+                 [__depthWall__, 12.23+2*__depthWall__ , __depthWall__],
+                 [__heightFloor__]
+               ]
+
+left_side = assemblyDiagramInit(shape)(sizePatterns)
+
+"""DOOR"""
+shape = [1,3,1]
+sizePatterns = [ [__depthWall__],
+                 [5.23, 2 , 5+2*__depthWall__],
+                 [__heightFloor__]
+               ]
+door = assemblyDiagramInit(shape)(sizePatterns)
+door = REMOVE_CELL(door,[1])
+"""WINDOW"""
+shape = [3,1,3]
+sizePatterns = [ [ 1.6, 1.04 , 0.2],
+                 [__depthWall__],
+                 [__heightFloor__*0.4,__heightFloor__*0.3,__heightFloor__*0.2]
+               ]
+window = assemblyDiagramInit(shape)(sizePatterns)
+window = REMOVE_CELL(window,[4])
+
+left_side = VMR_CELL(left_side,[door,window,window],[1,3,5],[4])
 
 ###############################################################################################
 #                                 RIGHT_SIDE
 ###############################################################################################
 
-shape = [1,5,1]
-sizePatterns = [ [6.24+__depthWall__],
-                 [4.6,__depthWall__,1.7,__depthWall__,5.93],
+
+shape = [2,3,1]
+sizePatterns = [ [__depthWall__,2.84],
+                 [__depthWall__, 12.23+2*__depthWall__ , __depthWall__],
                  [__heightFloor__]
                ]
 
 right_side = assemblyDiagramInit(shape)(sizePatterns)
 
-###############################################################################################
-#                                 RIGHT_TOP_SIDE
-###############################################################################################
 
-shape = [3,1,1]
-sizePatterns = [ [3.4,__depthWall__,2.84],
-                 [5.93],
-                 [__heightFloor__]
-               ]
-
-right_top_side = assemblyDiagramInit(shape)(sizePatterns)
-
-###############################################################################################
-#                                 RIGHT_TOP_LEFT_SIDE
-###############################################################################################
-
-shape = [1,3,1]
-sizePatterns = [ [3.4],
-                 [4.7,__depthWall__,1.16-__depthWall__],
-                 [__heightFloor__]
-               ]
-
-right_top_left_side = assemblyDiagramInit(shape)(sizePatterns)
-
-
-right_top_left_side = VMR_CELL(right_top_left_side,[],[],[0,2])
-VIEW_CELL(right_top_left_side)
-right_top_side      = VMR_CELL(right_top_side,[right_top_left_side],[0],[0,2])
-right_side          = VMR_CELL(right_side,[right_top_side],[4],[0,2])
-diagram_3D          = VMR_CELL(master,[right_side],[21],[9])
+diagram_3D          = VMR_CELL(master,[left_side],[1])
 
 DRAW(diagram_3D)
 
