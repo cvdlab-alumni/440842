7896c1c006ae428f91a8513bcfdc9e6e7bcb83db
AndreaOlivieri <oli.andrea17@gmail.com>
2014-05-17 15:01:37 +0200

    as

--- 2014-05-16/python/exercise2.py
+++ 2014-05-16/python/exercise2.py
@@ -10,25 +10,36 @@ __distanceBetweenApartment__ = 3
 #									FIRST FLOOR
 ##########################################################################################
 
-shape = [3,3,2]
-sizePatterns = [ [__depthWall__, 9.08+2*__depthWall__ ,__depthWall__],
-                 [__depthWall__,12.23+2*__depthWall__,__depthWall__],
-                 [__depthfloor__,__heightFloor__]
-               ]
-first_floor_diagram = assemblyDiagramInit(shape)(sizePatterns)
-door = ROTATE_DIAG(PI/2.)(MKDOOR(12.23+2*__depthWall__,5.23,2)) 
-first_floor_diagram = VMR_CELL(first_floor_diagram, [door], [3], [9])
+def build_first_floor():
+	shape = [3,3,2]
+	sizePatterns = [ [__depthWall__, 9.08+2*__depthWall__ ,__depthWall__],
+    	             [__depthWall__,12.23+2*__depthWall__,__depthWall__],
+        	         [__depthfloor__,__heightFloor__]
+            	   ]
+	first_floor_diagram = assemblyDiagramInit(shape)(sizePatterns)
+	door = ROTATE_DIAG(PI/2.)(MKDOOR(12.23+2*__depthWall__,5.23,2)) 
+	return VMR_CELL(first_floor_diagram, [door], [3], [9])
 
-first_floor = T(2)(5*__depthWall__)(STRUCT(MKPOLS(first_floor_diagram)))
-first_floor = STRUCT([ COMP([T([1])([-__distanceBetweenApartment__]),S([1])([-1])]) (first_floor) , first_floor ])
+def mkelevator():
+	shape = [3,3,2]
+	sizePatterns = [ [__depthWall__, __distanceBetweenApartment__-2*__depthWall__ ,__depthWall__],
+    	             [__depthWall__, __distanceBetweenApartment__-2*__depthWall__ ,__depthWall__],
+        	         [__depthfloor__,__heightFloor__]
+            	   ]
+	elevator = assemblyDiagramInit(shape)(sizePatterns)
+	door = (MKDOOR(__distanceBetweenApartment__-2*__depthWall__,(__distanceBetweenApartment__-2*__depthWall__)/2.-0.5,1)) 
+	return VMR_CELL(elevator, [door], [11], [9])
 
-VIEW(first_floor)
+DRAW(mkelevator())
 
-apartment = STRUCT(MKPOLS(build_apartment()))
-floor = T(3)(__depthfloor__+__heightFloor__)(STRUCT([ COMP([T([1])([-__distanceBetweenApartment__]),S([1])([-1])]) (apartment) , apartment ]))
+def mkbuilding():
+	first_floor = T(2)(5*__depthWall__)(STRUCT(MKPOLS(first_floor_diagram)))
+	first_floor = STRUCT([ COMP([T([1])([-__distanceBetweenApartment__]),S([1])([-1])]) (first_floor) , first_floor ])	
+	apartment = STRUCT(MKPOLS(build_apartment()))
+	floor = STRUCT([ COMP([T([1])([-__distanceBetweenApartment__]),S([1])([-1])]) (apartment) , apartment ])
+	floor = T(3)(__depthfloor__+__heightFloor__)(floor)
+	floors = STRUCT( NN(8)([floor, T([3])(__depthfloor__+__heightFloor__)]))
+	return STRUCT([first_floor, floors])  
 
-VIEW(floor)
 
-building = STRUCT([first_floor, STRUCT( NN(8)([floor, T([3])(__depthfloor__+__heightFloor__)]) ) ])  
-
-VIEW(building)
+VIEW(mkbuilding())
